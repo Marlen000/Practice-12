73feb150348aad1bd55d9d833d4706cacdf63a11
Marlen000 <marlen27082006@gmail.com>
2024-11-22 21:44:35 +0500

    Zhuban Marlen SIS-2302  IITU   Practice 12

---
+++ Main.java
@@ -0,0 +1,47 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.List;
+import java.util.ArrayList;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.apache.poi.ss.usermodel.*;
+
+public class Main {
+    public static void main(String[] args) {
+        String filePath = "C:\\Users\\marle\\IdeaProjects\\practice 12\\students.xlsx";
+
+        List<Student> students = readStudentsFromExcel(filePath);
+
+        for (Student student : students) {
+            System.out.printf("Имя: %s, Текущая стипендия: %.2f, Новая стипендия: %.2f, Увеличение: %.2f%n",
+                    student.getName(),
+                    student.getCurrentScholarship(),
+                    student.getNewScholarship(),
+                    student.getScholarshipIncrease());
+        }
+    }
+
+    public static List<Student> readStudentsFromExcel(String filePath) {
+        List<Student> students = new ArrayList<>();
+
+        try (FileInputStream fis = new FileInputStream(new File(filePath));
+             XSSFWorkbook workbook = new XSSFWorkbook(fis)) {
+
+            Sheet sheet = workbook.getSheetAt(0);
+
+            for (int i = 1; i <= sheet.getLastRowNum(); i++) {
+                Row row = sheet.getRow(i);
+
+                String name = row.getCell(0).getStringCellValue();
+                double currentScholarship = row.getCell(1).getNumericCellValue();
+                double newScholarship = row.getCell(2).getNumericCellValue();
+
+                students.add(new Student(name, currentScholarship, newScholarship));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return students;
+    }
+}
